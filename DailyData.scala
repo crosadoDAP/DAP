ea4f13abbc20a64402ef0fc448cbb3b0632daf74
Charlie Rosado <38768924+crosadoDAP@users.noreply.github.com>
2018-10-16 11:16:22 -0400

    Update and rename HuluDaily.scala to DailyData.scala

--- DailyData.scala
+++ DailyData.scala
@@ -7,12 +7,12 @@
   * Licensed under Creative Commons CC BY, CC BY-SA, CC0
   */
 
-/* Hulu data processing -
+/* Data processing -
 * creates a new column called 'service' that extracts the source of the feed from filename.
 * The only method to determine the source of the data (HBO vs Cinemax) is to extract the source from filename.
 * The process herein scans the filename, identifies whether HBO or Cinemax and then places the value in a new column.
 * Same rule is going to apply for partitioning.
-* This file impacts the HuluData set which represents Hulu_User_Daily
+* This file impacts a data set which represents Hulu_User_Daily
 */
 
 
